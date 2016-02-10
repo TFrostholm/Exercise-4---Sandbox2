c86b163d259b5f27c321085236264fba05f977e5
Toke Frostholm Rasmussen <toke1011@edu.easj.dk>
2016-02-10 16:04:33 +0100

    Initial commit and solved

---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            int a = 5 + 7;
+
+            int b = 25;
+            b = 3*b;
+
+            int c = 16/5;
+
+            double d = 2.5;
+            d = d/3.5;
+
+            int e = b - a;
+            e = e*c;
+
+            double f = 24%6;
+
+            double g = 35/f;
+
+            Console.WriteLine(f);
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
